e1d56433018493192e46a4ac001d75a713010245
Your Name <you@example.com>
2022-09-11 11:04:16 +0300

    add task25

---
+++ task25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+Console.Write("Введите число возводимое в степень: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите сепень: ");
+int degree = int.Parse(Console.ReadLine()!);
+double pow = 1;
+
+if (degree > 0)
+    for (int i = 1; i <= degree; i++)
+    {
+        pow = pow * number;
+    }
+else
+{
+    for (int i = 1; i <= Math.Abs(degree); i++)
+    {
+        pow = pow * number;
+    }
+    pow = 1 / pow;
+}
+Console.WriteLine($"число {number} возведенное в степень {degree} равно {pow}");
+
---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и 
+// выдаёт произведение чисел от 1 до N.
+
+int FokCount(int number)
+{
+    int fok = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        fok = fok * i;
+    }
+    return fok;
+}
+
+Console.Write("Введите N: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"фоктериал числа равен {FokCount(number)}");
