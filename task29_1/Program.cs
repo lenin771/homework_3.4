c5fe7ace605061cfcbf70ad204d7c26a80afc062
Your Name <you@example.com>
2022-09-13 09:03:21 +0300

    add task29_1

---
+++ task29_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 29: Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+
+Console.WriteLine("Введите 8 элементов массива через пробел");
+string elementsArray = Console.ReadLine();
+string[] elementsSplit = elementsArray.Split(' ');
+int[] array = new int[elementsArray.Length];
+
+for (int i = 0; i < elementsSplit.Length; i++)
+{
+    array[i] = int.Parse(elementsSplit[i]);
+    Console.Write($"{array[i]}{' '}");
+}
+Console.WriteLine();
